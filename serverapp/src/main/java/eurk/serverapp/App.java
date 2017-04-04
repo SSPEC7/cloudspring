8bd2f9f6300c1888bc795a8905f9b18e9eed6529
RiteshSingh9984 <shivritesh9984@gmail.com>
2017-04-04 12:40:32 +0530

    spring eureka server client and feign-client

---
+++ serverapp/src/main/java/eurk/serverapp/App.java
@@ -0,0 +1,13 @@
+package eurk.serverapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
